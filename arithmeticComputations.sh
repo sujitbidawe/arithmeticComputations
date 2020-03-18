6604d678ed58ed851f8d1b3f50a2178574499ee7
sujitbidawe <bidawesujit@gmail.com>
2020-03-18 21:49:58 +0530

    added: code to compute a%b+c

--- arithmeticComputations.sh
+++ arithmeticComputations.sh
@@ -3,11 +3,16 @@
 read -p "enter three numbers separated by space: " a b c
 echo "a=$a, b=$b, c=$c"
 
-result1=$(( a+b*c ))
+result1=$(( a + b * c ))
 echo "a+b*c=$result1"
 
-result2=$(( a*b+c ))
+result2=$(( a * b + c ))
 echo "a*b+c=$result2"
 
-result3=`echo "scale=4; $c+$a/$b" | bc`
+result3=`echo "scale=2; $c + $a / $b" | bc`
 echo "c+a/b=$result3"
+
+result4=$(( a % b + c ))
+echo "a%b+c=$result4"
+
+
