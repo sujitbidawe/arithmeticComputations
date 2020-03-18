f79b71c07841e0133bee7779b9c6a6b5249aa7bc
sujitbidawe <bidawesujit@gmail.com>
2020-03-18 23:49:25 +0530

    added: code for adding results in the dictionary

--- arithmeticComputations.sh
+++ arithmeticComputations.sh
@@ -1,18 +1,25 @@
-#! /bin/bash
+#!/usr/local/bin/bash
+
+declare -A resultDict
 
 read -p "enter three numbers separated by space: " a b c
 echo "a=$a, b=$b, c=$c"
 
 result1=$(( a + b * c ))
-echo "a+b*c=$result1"
+#echo "a+b*c=$result1"
 
 result2=$(( a * b + c ))
-echo "a*b+c=$result2"
+#echo "a*b+c=$result2"
 
-result3=`echo "scale=2; $c + $a / $b" | bc`
-echo "c+a/b=$result3"
+result3="`echo "scale=2; $c + $a / $b" | bc`"
+#echo "c+a/b=$result3"
 
 result4=$(( a % b + c ))
-echo "a%b+c=$result4"
+#echo "a%b+c=$result4"
 
+resultDict=( ["a+b*c"]=$result1 ["a*b+c"]=$result2 ["c+a/b"]=$result3 ["a%b+c"]=$result4 )
 
+for key in "${!resultDict[@]}"
+do
+		echo "$key=${resultDict[$key]}"
+done
